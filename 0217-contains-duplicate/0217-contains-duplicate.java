9fcc8e027ac6cb673223799c81c60e914c8b645e
Deblena Panja <DEBLENAPANJA@GMAIL.COM>
2026-08-30 23:04:59 +0530

    Time: 16 ms (89.18%), Space: 108.5 MB (20.91%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        java.util.HashSet<Integer> set = new java.util.HashSet<>();
+        for(int n : nums) if(!set.add(n)) return true;
+        return false;
+    }
+}
