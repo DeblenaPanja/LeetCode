d2064e83f2119f389333b5ec852f9b56aab4d58e
Deblena Panja <DEBLENAPANJA@GMAIL.COM>
2026-09-14 15:37:12 +0530

    Time: 54 ms (64.41%), Space: 68.8 MB (83.32%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,26 +1,18 @@
 import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
     public int totalFruit(int[] fruits) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-        int left = 0;
-        int maxFruits = 0;
-
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int left = 0, max = 0;
         for (int right = 0; right < fruits.length; right++) {
-            countMap.put(fruits[right], countMap.getOrDefault(fruits[right], 0) + 1);
-
-            while (countMap.size() > 2) {
-                countMap.put(fruits[left], countMap.get(fruits[left]) - 1);
-                if (countMap.get(fruits[left]) == 0) {
-                    countMap.remove(fruits[left]);
-                }
+            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+            if (map.size() > 2) {
+                map.put(fruits[left], map.get(fruits[left]) - 1);
+                if (map.get(fruits[left]) == 0) map.remove(fruits[left]);
                 left++;
             }
-
-            maxFruits = Math.max(maxFruits, right - left + 1);
+            max = Math.max(max, right - left + 1);
         }
-
-        return maxFruits;
+        return max;
     }
 }
