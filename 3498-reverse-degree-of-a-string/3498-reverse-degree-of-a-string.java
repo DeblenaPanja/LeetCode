db6b960a99f24fe079bb7ff53decff5b20a1ffaa
Deblena Panja <DEBLENAPANJA@GMAIL.COM>
2026-09-20 23:07:41 +0530

    Time: 1 ms (100%), Space: 44.3 MB (18.31%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int reversePos = 'z' - s.charAt(i) + 1;
+            sum += reversePos * (i + 1);
+        }
+        return sum;
+    }
+}
