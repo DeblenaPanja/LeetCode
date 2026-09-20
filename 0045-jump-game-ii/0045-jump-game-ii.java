938054178c9590f0d6e3272562b383f0e2ca3df6
Deblena Panja <DEBLENAPANJA@GMAIL.COM>
2026-09-20 23:10:35 +0530

    Time: 2 ms (29.83%), Space: 47.4 MB (21.39%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0, currentEnd = 0, furthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            furthest = Math.max(furthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = furthest;
+            }
+        }
+        return jumps;
+    }
+}
